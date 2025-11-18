25404f16900d36aefc412a03d7517e8ca909e9ed
juswamacbook <joshuaranin2@gmail.com>
2025-11-18 15:07:47 -0500

    palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = word
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphabet.includes(character))
+    .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
