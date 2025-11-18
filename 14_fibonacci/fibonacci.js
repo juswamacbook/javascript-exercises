181c81aaccde4938aa103ab42b6f22c69bf6e4a8
juswamacbook <joshuaranin2@gmail.com>
2025-11-18 15:57:45 -0500

    done without recursion

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
+
+    if (count < 0) {
+        return 'OOPS';
+    } if (count === 0) {
+        return 0;
+    }
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev
 
 };
 
