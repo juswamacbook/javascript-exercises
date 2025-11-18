b9f26b574461fa308481dddf941035cfd6fbbc47
juswamacbook <joshuaranin2@gmail.com>
2025-11-18 17:50:57 -0500

    find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return currentAge > oldestAge ? person : oldest;
+    });
 };
+
+function getAge(yearOfBirth, yearOfDeath) {
+    const currentYear = new Date().getFullYear();
+    const deathYear = yearOfDeath || currentYear;
+    return deathYear - yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
